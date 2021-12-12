453913fe9a010d73d0142f075b15b3f56ba3c8da
Flaviojcf <fjcf@poli.br>
2021-12-12 17:22:21 -0300

    Adicionando SignIn no Roteamento

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import {
     Routes,
     Route
 } from "react-router-dom";
+import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
 
 export function AppRoutes() {
@@ -10,6 +11,8 @@ export function AppRoutes() {
         <BrowserRouter>
             <Routes >
                 <Route path="/cadastro" element={<SignUp />}>
+                </Route>
+                <Route path="/login" element={<SignIn />}>
                 </Route>
             </Routes>
         </BrowserRouter>
