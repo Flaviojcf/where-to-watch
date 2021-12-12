124ad7bb2beef35eeba6dddc09d223bc274e4eb0
Flaviojcf <fjcf@poli.br>
2021-12-12 17:13:44 -0300

    SignIn

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,24 +1,29 @@
-import {Container} from './styles'
+import {Container, Content} from './styles'
 
 export function SignIn(){
     return (
         <Container>
+            <Content>
                 <div>
 
                      <h1>Sign In</h1>
                      <p>Login to manage your account</p>
-                     <input type="text" name="email" id="email" />
-                     <input type="text" name="password" id="password" />
+
+                     <input type="text" placeholder='Email' name="email" id="email" />
+                     <input type="text" placeholder="Password" name="password" id="password" />
+
                      <label><input type="checkbox"/>Remember me</label>
+
                      <button type='button'>Sign In</button>
-                <div className='informações'>
-                    <p>Don't have an account? Sign up</p>
-                    <p>Forgot password?</p>
+
+                <div className='adicionais'>
+                    <p>Don't have an account?<a href="#">Sign up</a></p>
+                    <a href="#"> Forgot a password? </a>
                     </div>
         
 
                 </div>
-
+                </Content>
         </Container>
     )
 }
--- src/components/SignIn/styles.ts
+++ src/components/SignIn/styles.ts
@@ -1,32 +1,101 @@
 import styled from "styled-components";
 
-
-export const Container = styled.header`
-    color: var(--text-title);
+export const Container = styled.div`
+    background: var(--background);
     display: flex;
-
-    div {
-        display: grid;
-        margin: auto;
-        text-align: center;
-}
-
-h1 {
-    margin-top: 1.8rem;
-} 
-
-p {
-    margin-top: 1.2rem;
-}
-
-input {
-    margin-top: 1.2rem;
-    border: none;
-    
-}
-
-.informações {
-    display: flex;
-    
-}
 `;
+
+export const Content = styled.main `
+    width: 100%;
+    max-width: 500px;
+    
+    margin: 8rem auto 0;
+    padding: 1rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+
+    h1 {
+        font-size: 30px;
+        font-weight: 600;
+        margin-bottom: 1rem;
+        color: var(--text-title);
+    }
+
+    p {
+        font-size: 1rem;
+        font-weight: 500;
+        margin-bottom: 1.5rem;
+        color: var(--text-body);
+    }
+
+    input {
+    height: 4rem;
+    padding: 0 1.5rem;
+    width: 100%;
+    border-radius: 0.5rem;
+    border: 0;
+    background: var(--input-background);
+    color: var(--text-title);
+
+    &+ input{
+        margin-top: 1rem;
+    }
+
+    &::placeholder {
+        color: var(--text-body)
+    }
+
+}
+
+
+label {
+        margin: 1rem 0 2rem 0;
+        width: 100%;
+      
+
+        color: var(--text-title);
+
+        }
+
+input[type="checkbox"] {
+            height: auto;
+            width: auto;
+            margin:  1rem 0.5rem 2rem 0;
+}
+
+    button {
+    width: 100%;
+    margin-bottom: 2rem;
+
+    height: 4rem;
+    padding: 0 1.5rem;
+
+    border: 0;
+    border-radius: 0.5rem;
+
+    background: var(--green);
+    color: #FFF;
+
+    font-weight: 500;
+
+    transition: filter 0.2s;
+
+    &:hover {
+        filter: brightness(0.9);
+    }
+
+}
+
+        a {
+            margin-left: 0.5rem;
+            text-decoration: none;
+            color: var(--blue);
+        }
+        
+.adicionais {
+    text-align: center;
+}
+`
