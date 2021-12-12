8d284e16e1f72bbf9cca15370dfb3a905aa05519
Eduardo Gomes <eduardogoomesf@hotmail.com>
2021-12-12 16:19:49 -0300

    Atualizando componentes Button e Input

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,13 @@ import { Container } from "./styles";
 interface ButtonProps {
     type: "button" | "submit" | "reset";
     title: string;
+    onClick: () => void;
+    disabled?: boolean;
 }
 
-export function Button({ title, type }: ButtonProps) {
+export function Button({ title, type, onClick, disabled = false }: ButtonProps) {
     return (
-        <Container type={type}>
+        <Container type={type} onClick={onClick} disabled={disabled}>
             {title}
         </Container>
     )
